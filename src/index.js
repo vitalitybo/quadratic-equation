0eef7ceb9fdde38d369ba238e5cd24d7dcb81f86
vitalitybo <v2.borisevich@gmail.com>
2018-09-19 19:55:52 +0300

    Quadratic-equation All tests passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let eqInArray = equation.split(" ");
+  let result = [];
+
+  if (~eqInArray[0].indexOf("-")) {
+    var a = 0 - (+eqInArray[0].slice(1));
+  } else {
+    var a = +eqInArray[0];
+  }
+
+  if (~eqInArray[3].indexOf("-")) {
+    var b = 0 - (+eqInArray[4]);
+  } else {
+    var b = +eqInArray[4];
+  }
+
+  if (~eqInArray[7].indexOf("-")) {
+    var c = 0 - (+eqInArray[8]);
+  } else {
+    var c = +eqInArray[8];
+  }
+ 
+  result.push( Math.round((-b) + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a));
+  result.push( Math.round((-b) - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a));
+
+  if (result[1] < result[0]) {
+    result.sort((right, left) => right - left);
+  }
+
+  return result;
 }
